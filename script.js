36b14de7485181098abc13322834d3100b10a168
Chris Young <chrisy1364@gmail.com>
2017-07-21 15:12:27 +0000

    boop

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
 
-  /*
-    $("ul li").click(function() {
-      var clicked_href = $(this).children('a').attr('href');
-      $('img').hide();
-      $(clicked_href).show();
-    });
-    */
+      /*
+        $("ul li").click(function() {
+          var clicked_href = $(this).children('a').attr('href');
+          $('img').hide();
+          $(clicked_href).show();
+        });
+        */
 
-  $('.carousel').carousel({
-    interval: 1000 * 10
-  });
+      $('.carousel').carousel({
+        interval: 1000 * 10
+      });
+
+      
+$("#submit").click(function(){
+   
+if (
+    $("#firstname").val() == "" ||
+    $("#lastname").val() == "" ||
+    $("#email").val() == "" ||
+    $("#card").val() == "" ||
+    $("#SSN").val() == "" 
+            ){
+            alert("Please fill all the sections.");
+            } else {
+            alert("Form is submitted. Thank you for helping the leaders of tommorow.");
+            }
+ });
 });
